ab25fd671eaf43bdc57b20227fed7bfd47d1be06
Lars Wefald <lars.wefald@embriq.no>
2020-11-26 18:03:35 +0100

    Committing stuff

--- cmd/write/main.go
+++ cmd/write/main.go
@@ -1,20 +1,97 @@
 package main
 
 import (
+	"bytes"
+	"crypto"
+	"crypto/rand"
+	"crypto/rsa"
+	"crypto/sha256"
+	"crypto/x509"
+	"encoding/pem"
+	"errors"
 	"fmt"
-	"github.com/larwef/lcoin/lcoin"
+	"github.com/larwef/lcoin/user"
+	"log"
 )
 
+type Transaction struct {
+	Message             string
+	RecipientPubKeyHash [32]byte
+}
+
+func (t *Transaction) Hash() [32]byte {
+	h1 := sha256.Sum256(append([]byte(t.Message), t.RecipientPubKeyHash[:]...))
+	return sha256.Sum256(h1[:])
+}
+
+func (t *Transaction) Unlock(pubKey *rsa.PublicKey, signature []byte) error {
+	var pubKeyPem bytes.Buffer
+	if err := pem.Encode(&pubKeyPem, &pem.Block{
+		Bytes: x509.MarshalPKCS1PublicKey(pubKey),
+	}); err != nil {
+		return err
+	}
+
+	pubKeyHash := sha256.Sum256(pubKeyPem.Bytes())
+	if !bytes.Equal(t.RecipientPubKeyHash[:], pubKeyHash[:]) {
+		return errors.New("not a matching public key")
+	}
+
+	t1Hash := t.Hash()
+	return rsa.VerifyPKCS1v15(pubKey, crypto.SHA256, t1Hash[:], signature)
+}
+
 func main() {
+	user1 := user.LoadUser("user1")
+	user2 := user.LoadUser("user2")
+	//user3 := user.LoadUser("user3")
 
-	var transactions []string
-	for i := 0; i < 16; i++ {
-		transactions = append(transactions, fmt.Sprintf("Transaction %d", i))
+	var user1PublicKey bytes.Buffer
+	err := pem.Encode(&user1PublicKey, &pem.Block{
+		Bytes: x509.MarshalPKCS1PublicKey(&user1.PrivateKey.PublicKey),
+	})
+	checkErr(err)
+
+	t1 := &Transaction{
+		Message:             "someone gives 1 token",
+		RecipientPubKeyHash: sha256.Sum256(user1PublicKey.Bytes()),
 	}
 
-	b1 := &lcoin.Block{
-		Transactions: transactions,
+	t1Hash := t1.Hash()
+
+	// Unlock with user1
+	sign1, err := rsa.SignPKCS1v15(rand.Reader, user1.PrivateKey, crypto.SHA256, t1Hash[:])
+	checkErr(err)
+
+	if err := t1.Unlock(&user1.PrivateKey.PublicKey, sign1); err != nil {
+		fmt.Printf("Error unlocking t1 with user1: %v.\n", err)
+	} else {
+		fmt.Println("User1 sucessfully unlocked t1.")
 	}
 
-	b1.MerkleRoot()
+	// Unlock with user2 using own pubkey
+	sign21, err := rsa.SignPKCS1v15(rand.Reader, user2.PrivateKey, crypto.SHA256, t1Hash[:])
+	checkErr(err)
+
+	if err := t1.Unlock(&user2.PrivateKey.PublicKey, sign21); err != nil {
+		fmt.Printf("Error unlocking t1 with user2: %v.\n", err)
+	} else {
+		fmt.Println("User2 sucessfully unlocked t1.")
+	}
+
+	// Unlock with user2 using user1 pubkey
+	sign22, err := rsa.SignPKCS1v15(rand.Reader, user2.PrivateKey, crypto.SHA256, t1Hash[:])
+	checkErr(err)
+
+	if err := t1.Unlock(&user1.PrivateKey.PublicKey, sign22); err != nil {
+		fmt.Printf("Error unlocking t1 with user2: %v.\n", err)
+	} else {
+		fmt.Println("User2 sucessfully unlocked t1.")
+	}
+}
+
+func checkErr(err error) {
+	if err != nil {
+		log.Fatal(err)
+	}
 }
--- lcoin/block.go
+++ lcoin/block.go
@@ -2,40 +2,88 @@ package lcoin
 
 import (
 	"crypto/sha256"
+	"encoding/binary"
 	"encoding/hex"
-	"fmt"
-	"github.com/larwef/lcoin/merkle"
 )
 
+type Chain struct {
+	Root *Block
+}
+
+func (c *Chain) Add(b *Block) {
+
+}
+
 type Header struct {
-	PrevHash   string `json:"prevHash"`
-	MerkleRoot string `json:"merkleRoot"`
-	Time       int64  `json:"time"`
-	Nonce      int64  `json:"nonce"`
+	PrevBlockHash string `json:"prevHash"`
+	MerkleRoot    string `json:"merkleRoot"`
+	Time          int64  `json:"time"`
+	Nonce         int64  `json:"nonce"`
+}
+
+func (h *Header) Hash() ([32]byte, error) {
+	hsh := sha256.New()
+	prev, err := hex.DecodeString(h.PrevBlockHash)
+	if err != nil {
+		return [32]byte{}, err
+	}
+	root, err := hex.DecodeString(h.MerkleRoot)
+	if err != nil {
+		return [32]byte{}, err
+	}
+
+	var t []byte
+	binary.BigEndian.PutUint64(t, uint64(h.Time))
+
+	var nonce []byte
+	binary.BigEndian.PutUint64(nonce, uint64(h.Nonce))
+
+	hsh.Write(prev)
+	hsh.Write(root)
+	hsh.Write(t)
+	hsh.Write(nonce)
+
+	return sha256.Sum256(hsh.Sum(nil)), nil
+}
+
+type Input struct {
+	PrevTX    string `json:"prevTX"`
+	Index     int    `json:"index"`
+	ScriptSig string `json:"scriptSig"`
+}
+
+type Output struct {
+	Value           int    `json:"value"`
+	ScriptPublicKey string `json:"scriptPublicKey"`
+}
+
+type Transaction struct {
+	Inputs  []*Input  `json:"inputs"`
+	Outputs []*Output `json:"outputs"`
+}
+
+func (t *Transaction) Hash() [32]byte {
+	h := sha256.New()
+
+	for _, elem := range t.Inputs {
+		h.Write([]byte(elem.PrevTX))
+		var index []byte
+		binary.BigEndian.PutUint64(index, uint64(elem.Index))
+		h.Write(index)
+		h.Write([]byte(elem.ScriptSig))
+	}
+
+	for _, elem := range t.Outputs {
+		var value []byte
+		binary.BigEndian.PutUint64(value, uint64(elem.Value))
+		h.Write(value)
+		h.Write([]byte(elem.ScriptPublicKey))
+	}
+
+	return sha256.Sum256(h.Sum(nil))
 }
 
 type Block struct {
-	Header       Header   `json:"header"`
-	Transactions []string `json:"transactions"`
-}
-
-func (b *Block) AddTransaction(transaction string) {
-	b.Transactions = append(b.Transactions, transaction)
-}
-
-func (b *Block) MerkleRoot() string {
-	var hashes [][32]byte
-	for _, elem := range b.Transactions {
-		hashes = append(hashes, sha256.Sum256([]byte(elem)))
-	}
-
-	_ = merkle.NewTree(hashes)
-
-	return ""
-}
-
-func printHasehs(hashes [][]byte) {
-	for _, elem := range hashes {
-		fmt.Println(hex.EncodeToString(elem))
-	}
+	Header       Header         `json:"header"`
+	Transactions []*Transaction `json:"transactions"`
 }
